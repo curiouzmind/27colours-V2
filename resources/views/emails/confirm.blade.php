992f2f79562a4563a2dee0dfad697e51d72404c4
Samuel-Junior Oghogho <samizares@hotmail.com>
2016-06-09 02:25:51 +0100

    login confirm text

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -6,17 +6,16 @@
             <div class="col-md-4 col-md-offset-7">
                 <div class="panel panel-default">
                     <div class="panel-heading"><h4>You can't perform some cool stuffs until you confirm your Account </h4>
-                    <p>Please, check your Email inbox/junk/spam folders for your activation email.</p></div>
+                        <p>Please, check your Email inbox/junk/spam folders for your activation email.</p>
+                        <a class="btn btn-danger btn-md" href="/profile"><i class="fa fa-btn fa-envelope"></i> Back to profile</a>
+                    </div>
                     <div class="panel-body">
                         <p>If you still can't find your email, click the resend link below</p>
-                        <a class="btn btn-danger btn-md" href="/profile"><i class="fa fa-btn fa-envelope"></i> Back to profile</a>
-
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">                           
-                                   <a class="btn btn-danger btn-md" href="/confirmation/resend"><i class="fa fa-btn fa-envelope"></i> Resend email</a>
+                                                         
+                        <a class="btn btn-danger btn-md" href="/confirmation/resend"><i class="fa fa-btn fa-envelope"></i> Resend email</a>
                                     
-                                </div>
-                            </div>
+                                
+                            
                        <!-- </form> -->
                     </div>
                 </div>
